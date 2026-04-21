745aee0dc9dff97d6f4f1762b648a29c25ac5771
PyPylia <lia.mccracken@premiertech.com.au>
2026-04-22 09:35:09 +1000

    fix(Ruckus): Fix config backup is device isn't master

--- src/device/Ruckus.cs
+++ src/device/Ruckus.cs
@@ -13,10 +13,11 @@ public static class Ruckus
         Credential credential = device.GetAdminLogin();
         HttpResponseMessage response;
 
-        // Create HTTP Client that ignores invalid SSL certificates
+        // Create HTTP Client that ignores invalid SSL certificates, and disables auto-redirects
         // This is needed as the dashboard has an invalid SSL certificate
         HttpClientHandler httpHandler = new()
         {
+            AllowAutoRedirect = false,
             ClientCertificateOptions = ClientCertificateOption.Manual,
             ServerCertificateCustomValidationCallback = (_, _, _, _) => true
         };
@@ -25,13 +26,29 @@ public static class Ruckus
             BaseAddress = credential.GetBaseUri("https")
         };
 
+        // Check if this is the master (will redirect to master otherwise)
+        response = await httpClient.GetAsync("");
+        // Ignore OK status codes since they mean we're already on the master
+        if (response.StatusCode == HttpStatusCode.OK) { }
+        else if (response.StatusCode == HttpStatusCode.Redirect && response.Headers.Location is not null)
+            // We've got a redirect to the master, so set that as the new base address
+            httpClient = new()
+            {
+                BaseAddress = new($"https://{response.Headers.Location.Host}/")
+            };
+        else
+            throw new Exception($"Could not get index, got {(int)response.StatusCode} {response.ReasonPhrase}");
+
         // Login to get session token
         response = await httpClient.PostAsync("admin/login.jsp", new FormUrlEncodedContent([
             KeyValuePair.Create("username", credential.Username),
             KeyValuePair.Create("password", credential.Password),
             KeyValuePair.Create("ok", "Log\u00A0in"),
         ]));
-        if (response.StatusCode != HttpStatusCode.OK)
+        // Redirect means correct credentials in this instance
+        if (response.StatusCode == HttpStatusCode.OK)
+            throw new Exception("Login credentials invalid.");
+        else if (response.StatusCode != HttpStatusCode.Redirect)
             throw new Exception($"Could not get session token, got {(int)response.StatusCode} {response.ReasonPhrase}");
 
         // Download backup
