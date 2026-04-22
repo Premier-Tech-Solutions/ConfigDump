53d93d133d3ac777bf7b393786ccf988cdb0233a
PyPylia <lia.mccracken@premiertech.com.au>
2026-04-22 11:58:57 +1000

    fix(Ricoh): Change post bodies to form url encoded rather than JSON, and add timeout and extra error handling in configuration export status block

--- src/device/Ricoh.cs
+++ src/device/Ricoh.cs
@@ -1,7 +1,7 @@
 namespace ConfigDump.Device;
 
 using System.Net;
-using System.Net.Http.Json;
+using System.Text;
 
 public static class Ricoh
 {
@@ -23,18 +23,16 @@ public static class Ricoh
             BaseAddress = new("http://" + device.PrimaryIp + "/web/guest/en/websys/")
         };
 
-
         // Attempt to log in, trying again with default credentials if it doesn't work
         bool loggedIn = false;
         if (device.Credentials.Count > 0)
         {
             // Try logging in with credentials provided
             Credential credential = device.GetAdminLogin();
-            response = await httpClient.PostAsJsonAsync("webArch/login.cgi", new
-            {
-                userid = credential.Username,
-                credential.Password,
-            });
+            response = await httpClient.PostAsync("webArch/login.cgi", new FormUrlEncodedContent([
+                KeyValuePair.Create("userid", Convert.ToBase64String(Encoding.UTF8.GetBytes(credential.Username))),
+                KeyValuePair.Create("password", Convert.ToBase64String(Encoding.UTF8.GetBytes(credential.Password))),
+            ]));
             // Redirect means correct credentials in this instance
             if (response.StatusCode == HttpStatusCode.Redirect)
                 loggedIn = true;
@@ -45,11 +43,10 @@ public static class Ricoh
         if (!loggedIn)
         {
             // Try again with default credentials
-            response = await httpClient.PostAsJsonAsync("webArch/login.cgi", new
-            {
-                userid = "admin",
-                password = "",
-            });
+            response = await httpClient.PostAsync("webArch/login.cgi", new FormUrlEncodedContent([
+                KeyValuePair.Create("userid", "YWRtaW4="), // "admin" in base64
+                KeyValuePair.Create("password", ""),
+            ]));
             // Redirect means correct credentials in this instance
             if (response.StatusCode == HttpStatusCode.OK)
                 throw new Exception($"No valid login credentials.");
@@ -74,59 +71,62 @@ public static class Ricoh
             throw new Exception("Could not find wimToken.");
 
         // Setup configuration export
-        response = await httpClient.PostAsJsonAsync("prefsImpExp/setPrefsImpExp.cgi", new
-        {
-            mode = "EXPORT",
-            accessConf = 3,
-            rwInfo = 3,
-            expMachUniqInfo = true,
-            impMachUniqInfo = false,
-            impLogDownload = false,
-            passwdKind = "",
-            paramControl = "",
-            wayTo = "",
-            wimToken,
-        });
+        response = await httpClient.PostAsync("prefsImpExp/setPrefsImpExp.cgi", new FormUrlEncodedContent([
+            KeyValuePair.Create("wimToken", wimToken),
+            KeyValuePair.Create("mode", "EXPORT"),
+            KeyValuePair.Create("accessConf", "3"),
+            KeyValuePair.Create("rwInfo", "3"),
+            KeyValuePair.Create("expMachUniqInfo", "true"),
+            KeyValuePair.Create("impMachUniqInfo", "false"),
+            KeyValuePair.Create("impLogDownload", "false"),
+            KeyValuePair.Create("passwdKind", ""),
+            KeyValuePair.Create("paramControl", ""),
+            KeyValuePair.Create("wayTo", ""),
+        ]));
         if (response.StatusCode != HttpStatusCode.OK)
             throw new Exception($"Could not set-up configuration export, got {(int)response.StatusCode} {response.ReasonPhrase}");
 
         // Start export of configuration
-        response = await httpClient.PostAsJsonAsync("prefsImpExp/doPrefsExport.cgi", new
-        {
-            mode = "EXPORT",
-            importFile = "",
-            wimToken,
-        });
+        response = await httpClient.PostAsync("prefsImpExp/doPrefsExport.cgi", new FormUrlEncodedContent([
+            KeyValuePair.Create("wimToken", wimToken),
+            KeyValuePair.Create("mode", "EXPORT"),
+            KeyValuePair.Create("importFile", ""),
+        ]));
         if (response.StatusCode != HttpStatusCode.OK)
             throw new Exception($"Could not start configuration export, got {(int)response.StatusCode} {response.ReasonPhrase}");
 
-        // Wait until export is done
-        do
+        // Wait until export is done, or 120 seconds
+        for (int i = 0; i < 120; i++)
         {
-            response = await httpClient.PostAsJsonAsync("prefsImpExp/getPrefsImpExpProgress.cgi", new
-            {
-                mode = "EXPORT",
-                paramControl = "",
-                wayTo = "",
-                wimToken,
-            });
+            response = await httpClient.PostAsync("prefsImpExp/getPrefsImpExpProgress.cgi", new FormUrlEncodedContent([
+                KeyValuePair.Create("wimToken", wimToken),
+                KeyValuePair.Create("mode", "EXPORT"),
+                KeyValuePair.Create("paramControl", ""),
+                KeyValuePair.Create("wayTo", ""),
+            ]));
             if (response.StatusCode != HttpStatusCode.OK)
                 throw new Exception($"Could not get configuration export status, got {(int)response.StatusCode} {response.ReasonPhrase}");
 
+            string content = await response.Content.ReadAsStringAsync();
+            if (!(content.StartsWith('[') && content.EndsWith(']')))
+                throw new Exception("Got unexpected error while waiting for configuration export.");
+            // Break early if complete
+            else if (content == "[100]")
+                break;
+
             // Wait a second before checking again
             await Task.Delay(1000);
-        } while (await response.Content.ReadAsStringAsync() != "[100]");
+        }
 
         // Download configuration
-        response = await httpClient.PostAsJsonAsync("prefsImpExp/getPrefsExportData.cgi", new
-        {
-            mode = "EXPORT",
-            impLogDownload = false,
-            resultErrorMessage = "",
-            paramControl = "",
-            wayTo = "",
-            wimToken,
-        });
+        response = await httpClient.PostAsync("prefsImpExp/getPrefsExportData.cgi", new FormUrlEncodedContent([
+            KeyValuePair.Create("wimToken", wimToken),
+            KeyValuePair.Create("mode", "EXPORT"),
+            KeyValuePair.Create("impLogDownload", "false"),
+            KeyValuePair.Create("resultErrorMessage", ""),
+            KeyValuePair.Create("paramControl", ""),
+            KeyValuePair.Create("wayTo", ""),
+        ]));
         if (response.StatusCode != HttpStatusCode.OK)
             throw new Exception($"Could not download configuration, got {(int)response.StatusCode} {response.ReasonPhrase}");
 
